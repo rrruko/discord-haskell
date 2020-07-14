8f625096b2851a936d840292a9fccea0270b9730
Ethan Jones <joneetha@oregonstate.edu>
2020-07-13 23:03:57 -0700

    add parent id field and make position always optional

--- src/Discord/Internal/Types/Channel.hs
+++ src/Discord/Internal/Types/Channel.hs
@@ -24,11 +24,12 @@ data Channel
                                           --   the guild if it's the "general" channel).
       , channelGuild       :: GuildId     -- ^ The id of the guild.
       , channelName        :: T.Text      -- ^ The name of the guild (2 - 1000 characters).
-      , channelPosition    :: Integer     -- ^ The storing position of the channel.
+      , channelPosition    :: Maybe Integer     -- ^ The storing position of the channel.
       , channelPermissions :: [Overwrite] -- ^ An array of permission 'Overwrite's
       , channelUserRateLimit :: Integer   -- ^ Seconds before a user can speak again
       , channelNSFW        :: Bool        -- ^ Is not-safe-for-work
       , channelTopic       :: T.Text      -- ^ The topic of the channel. (0 - 1024 chars).
+      , channelParentId    :: Maybe ChannelId
       , channelLastMessage :: Maybe MessageId   -- ^ The id of the last message sent in the
                                                 --   channel
       }
@@ -36,18 +37,20 @@ data Channel
       { channelId          :: ChannelId
       , channelGuild       :: GuildId
       , channelName        :: T.Text
-      , channelPosition    :: Integer
+      , channelPosition    :: Maybe Integer
       , channelPermissions :: [Overwrite]
       , channelNSFW        :: Bool
       , channelTopic       :: T.Text
+      , channelParentId    :: Maybe ChannelId
       , channelLastMessage :: Maybe MessageId
       }
   | ChannelStorePage
       { channelId          :: ChannelId
       , channelGuild       :: GuildId
       , channelName        :: T.Text
-      , channelPosition    :: Integer
+      , channelPosition    :: Maybe Integer
       , channelNSFW        :: Bool
+      , channelParentId    :: Maybe ChannelId
       , channelPermissions :: [Overwrite]
       }
   -- | A voice channel in a guild.
@@ -55,10 +58,11 @@ data Channel
       { channelId          :: ChannelId
       , channelGuild       :: GuildId
       , channelName        :: T.Text
-      , channelPosition    :: Integer
+      , channelPosition    :: Maybe Integer
       , channelPermissions :: [Overwrite]
       , channelNSFW        :: Bool
       , channelBitRate     :: Integer     -- ^ The bitrate (in bits) of the channel.
+      , channelParentId    :: Maybe ChannelId
       , channelUserLimit   :: Integer     -- ^ The user limit of the voice channel.
       }
   -- | DM Channels represent a one-to-one conversation between two users, outside the scope
@@ -66,15 +70,20 @@ data Channel
   | ChannelDirectMessage
       { channelId          :: ChannelId
       , channelRecipients  :: [User]      -- ^ The 'User' object(s) of the DM recipient(s).
+      , channelParentId    :: Maybe ChannelId
       , channelLastMessage :: Maybe MessageId
       }
   | ChannelGroupDM
       { channelId          :: ChannelId
       , channelRecipients  :: [User]
+      , channelParentId    :: Maybe ChannelId
       , channelLastMessage :: Maybe MessageId
       }
   | ChannelGuildCategory
       { channelId          :: ChannelId
+      , channelPosition    :: Maybe Integer
+      , channelName        :: T.Text
+      , channelParentId    :: Maybe ChannelId
       , channelGuild       :: GuildId
       } deriving (Show, Eq, Ord)
 
@@ -86,47 +95,56 @@ instance FromJSON Channel where
         ChannelText  <$> o .:  "id"
                      <*> o .:? "guild_id" .!= 0
                      <*> o .:  "name"
-                     <*> o .:  "position"
+                     <*> o .:? "position"
                      <*> o .:  "permission_overwrites"
                      <*> o .:  "rate_limit_per_user"
                      <*> o .:? "nsfw" .!= False
                      <*> o .:? "topic" .!= ""
+                     <*> o .:? "parent_id"
                      <*> o .:? "last_message_id"
       1 ->
         ChannelDirectMessage <$> o .:  "id"
                              <*> o .:  "recipients"
+                             <*> o .:? "parent_id"
                              <*> o .:? "last_message_id"
       2 ->
         ChannelVoice <$> o .:  "id"
                      <*> o .:? "guild_id" .!= 0
                      <*> o .:  "name"
-                     <*> o .:  "position"
+                     <*> o .:? "position"
                      <*> o .:  "permission_overwrites"
                      <*> o .:? "nsfw" .!= False
                      <*> o .:  "bitrate"
+                     <*> o .:? "parent_id"
                      <*> o .:  "user_limit"
       3 ->
         ChannelGroupDM <$> o .:  "id"
                        <*> o .:  "recipients"
+                       <*> o .:? "parent_id"
                        <*> o .:? "last_message_id"
       4 ->
         ChannelGuildCategory <$> o .: "id"
+                             <*> o .:? "position"
+                             <*> o .: "name"
+                             <*> o .:? "parent_id"
                              <*> o .:? "guild_id" .!= 0
       5 ->
         ChannelNews <$> o .:  "id"
                     <*> o .:? "guild_id" .!= 0
                     <*> o .:  "name"
-                    <*> o .:  "position"
+                    <*> o .:? "position"
                     <*> o .:  "permission_overwrites"
                     <*> o .:? "nsfw" .!= False
                     <*> o .:? "topic" .!= ""
+                    <*> o .:? "parent_id"
                     <*> o .:? "last_message_id"
       6 ->
         ChannelStorePage <$> o .:  "id"
                          <*> o .:? "guild_id" .!= 0
                          <*> o .:  "name"
-                         <*> o .:  "position"
+                         <*> o .:? "position"
                          <*> o .:? "nsfw" .!= False
+                         <*> o .:? "parent_id"
                          <*> o .:  "permission_overwrites"
       _ -> fail ("Unknown channel type:" <> show type')
 
@@ -135,21 +153,23 @@ instance ToJSON Channel where
               [ ("id",     toJSON <$> pure channelId)
               , ("guild_id", toJSON <$> pure channelGuild)
               , ("name",  toJSON <$> pure channelName)
-              , ("position",   toJSON <$> pure channelPosition)
+              , ("position",   toJSON <$> channelPosition)
               , ("rate_limit_per_user", toJSON <$> pure channelUserRateLimit)
               , ("nsfw", toJSON <$> pure channelNSFW)
               , ("permission_overwrites",   toJSON <$> pure channelPermissions)
               , ("topic",   toJSON <$> pure channelTopic)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("last_message_id",  toJSON <$> channelLastMessage)
               ] ]
   toJSON ChannelNews{..} = object [(name,value) | (name, Just value) <-
               [ ("id",     toJSON <$> pure channelId)
               , ("guild_id", toJSON <$> pure channelGuild)
               , ("name",  toJSON <$> pure channelName)
-              , ("position",   toJSON <$> pure channelPosition)
+              , ("position",   toJSON <$> channelPosition)
               , ("permission_overwrites",   toJSON <$> pure channelPermissions)
               , ("nsfw", toJSON <$> pure channelNSFW)
               , ("topic",   toJSON <$> pure channelTopic)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("last_message_id",  toJSON <$> channelLastMessage)
               ] ]
   toJSON ChannelStorePage{..} = object [(name,value) | (name, Just value) <-
@@ -157,32 +177,39 @@ instance ToJSON Channel where
               , ("guild_id", toJSON <$> pure channelGuild)
               , ("name",  toJSON <$> pure channelName)
               , ("nsfw", toJSON <$> pure channelNSFW)
-              , ("position",   toJSON <$> pure channelPosition)
+              , ("position",   toJSON <$> channelPosition)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("permission_overwrites",   toJSON <$> pure channelPermissions)
               ] ]
   toJSON ChannelDirectMessage{..} = object [(name,value) | (name, Just value) <-
               [ ("id",     toJSON <$> pure channelId)
               , ("recipients",   toJSON <$> pure channelRecipients)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("last_message_id",  toJSON <$> channelLastMessage)
               ] ]
   toJSON ChannelVoice{..} = object [(name,value) | (name, Just value) <-
               [ ("id",     toJSON <$> pure channelId)
               , ("guild_id", toJSON <$> pure channelGuild)
               , ("name",  toJSON <$> pure channelName)
-              , ("position",   toJSON <$> pure channelPosition)
+              , ("position",   toJSON <$> channelPosition)
               , ("nsfw", toJSON <$> pure channelNSFW)
               , ("permission_overwrites",   toJSON <$> pure channelPermissions)
               , ("bitrate",   toJSON <$> pure channelBitRate)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("user_limit",  toJSON <$> pure channelUserLimit)
               ] ]
   toJSON ChannelGroupDM{..} = object [(name,value) | (name, Just value) <-
               [ ("id",     toJSON <$> pure channelId)
               , ("recipients",   toJSON <$> pure channelRecipients)
+              , ("parent_id", toJSON <$> channelParentId)
               , ("last_message_id",  toJSON <$> channelLastMessage)
               ] ]
   toJSON ChannelGuildCategory{..} = object [(name,value) | (name, Just value) <-
               [ ("id",     toJSON <$> pure channelId)
               , ("guild_id", toJSON <$> pure channelGuild)
+              , ("name", toJSON <$> pure channelName)
+              , ("parent_id", toJSON <$> channelParentId)
+              , ("position", toJSON <$> channelPosition)
               ] ]
 
 -- | If the channel is part of a guild (has a guild id field)
